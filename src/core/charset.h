74d683b4feae44eb288a0f0ca0c90b6e20c08cc3
Ondrej Mosnáček <omosnacek@gmail.com>
2015-11-01 14:46:49 +0100

    Improved documentation.

--- src/core/charset.h
+++ src/core/charset.h
@@ -14,6 +14,10 @@ namespace core {
 /**
  * A helper class for testing whether a character belongs to a set
  * in an efficient manner (using std::bitset).
+ *
+ * This class uses a CHAR_MAX-bit bitset to track which character belongs
+ * to the set and which doesn't. A character with value `x` belongs to
+ * the set if and only if the `x`-th bit is set in the underlying bitset.
  */
 class CharSet
 {
@@ -43,41 +47,54 @@ private:
     }
 
 public:
+    /** Constructs an empty set. */
     CharSet() noexcept
         : bs()
     {
     }
 
+    /** Constructs a set containing a single character. */
     CharSet(char c) noexcept
         : bs(bitset(1) << static_cast<unsigned char>(c))
     {
     }
 
+    /**
+     * Constructs a set containing a range of characters.
+     *
+     * @param s lower bound of the range (inclusive)
+     * @param e upper bound of the range (inclusive)
+     */
     CharSet(char s, char e)
         : bs(bs_range(s, e))
     {
     }
 
+    /** Returns true if the character belongs to the set, otherwise false. */
     bool match(char c) const noexcept
     {
         return bs.test(static_cast<unsigned char>(c));
     }
 
+    /** Returns the complement of the set. */
     CharSet operator~() const noexcept
     {
         return ~bs;
     }
 
+    /** Returns the union with another set. */
     CharSet operator|(const CharSet &other) const noexcept
     {
         return bs | other.bs;
     }
 
+    /** Returns the intersection with another set. */
     CharSet operator&(const CharSet &other) const noexcept
     {
         return bs & other.bs;
     }
 
+    /** Returns the symmetric difference with another set. */
     CharSet operator^(const CharSet &other) const noexcept
     {
         return bs ^ other.bs;
