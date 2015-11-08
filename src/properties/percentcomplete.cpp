14687770c9f9a620a18878c661378c7ae3b199fb
lenka.kun <lenka.kun@gmail.com>
2015-11-08 18:14:28 +0100

    properties: PERCENT,PRIORITY,STATUS: done

--- src/properties/percentcomplete.cpp
+++ src/properties/percentcomplete.cpp
@@ -1,14 +1,22 @@
 #include "percentcomplete.h"
+#include "core/valueparser.h"
 
 namespace ical {
 namespace properties {
 
 void PercentComplete::print(std::ostream &out) const {
-
+    out << "PERCENT-COMPLETE:" << value << "\r\n";
 }
 
 PercentComplete PercentComplete::parse(const core::WithPos<core::GenericProperty> &generic) {
-    return {};
+    if (!generic->getParameters().empty()) {
+        throw ParserException(generic.pos(), "The PERCENT-COMPLETE property must have no parameters!");
+    }
+
+    auto &value = generic->getValue();
+    PercentComplete perc;
+    perc.value = std::move(core::ValueParser::parseInteger(value.pos(), value->begin(), value->end()));
+    return perc;
 }
 
 } // namespace properties
--- src/properties/status.cpp
+++ src/properties/status.cpp
@@ -1,14 +1,26 @@
 #include "status.h"
+#include "core/valueparser.h"
 
 namespace ical {
 namespace properties {
 
 void Status::print(std::ostream &out) const {
-
+    out << "STATUS:" << value << "\r\n";
 }
 
 Status Status::parse(const core::WithPos<core::GenericProperty> &generic) {
-    return {};
+    if (!generic->getParameters().empty()) {
+        throw ParserException(generic.pos(), "The STATUS property must have no parameters!");
+
+    auto &value = generic->getValue();
+    if (*value != "AUDIO" && *value != "DISPLAY" && *value != "EMAIL") {
+            throw ParserException(value.pos(), "Invalid STATUS property value!");
+            }
+        Status status;
+        status.value = std::move(core::ValueParser::parseText(
+                                      value.pos(), value->begin(), value->end()));
+        return status;
+    }
 }
 
 } // namespace properties
