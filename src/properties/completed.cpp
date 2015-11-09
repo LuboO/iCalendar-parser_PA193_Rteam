0093b83217ed47695a86915b1c3e8ec5d843217c
Ondrej Mosnáček <omosnacek@gmail.com>
2015-11-09 22:01:57 +0100

    Adapted Lenka's properties to new DateTime parse method.
    Also added a missing parameter count check.

--- src/properties/completed.cpp
+++ src/properties/completed.cpp
@@ -1,21 +1,27 @@
 #include "completed.h"
-#include "core/valueparser.h"
-
 
 namespace ical {
 namespace properties {
 
-void Completed::print(std::ostream &out) const{
+void Completed::print(std::ostream &out) const
+{
     out << "COMPLETED:";
     value.print(out);
     out << "\r\n";
 
 }
 
-Completed Completed::parse(const core::WithPos<core::GenericProperty> &generic) {
+Completed Completed::parse(const core::WithPos<core::GenericProperty> &generic)
+{
+    if (!generic->getParameters().empty()) {
+        throw ParserException(generic.pos(), "The RRULE property must have no parameters!");
+    }
+
     auto &value = generic->getValue();
     Completed completed;
-    completed.value = std::move(core::ValueParser::parseDateTime(value.pos(), value->begin(), value->end()));
+    completed.value = std::move(
+                data::DateTime::parse(
+                    value.pos(), value->begin(), value->end()));
     return completed;
 }
 
--- src/properties/dtend.cpp
+++ src/properties/dtend.cpp
@@ -42,7 +42,9 @@ Dtend Dtend::parse(const core::WithPos<core::GenericProperty> &generic) {
     else if (dtend.valueParam[0].getValue() == "DATE-TIME") justDate = false;
     else throw ParserException(generic.pos() , "invalid DTEND  VALUE parameter can be only DATE or DATE-TIME");
 
-    dtend.value = std::move(core::ValueParser::parseDateTime(value.pos(), value->begin(), value->end(),justDate));
+    dtend.value = std::move(
+                data::DateTime::parse(
+                    value.pos(), value->begin(), value->end(), justDate));
 
     return dtend;
 }
