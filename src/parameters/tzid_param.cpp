e8875a09a29e29606b4441b2142ec8130ec1a360
Ondrej Mosnáček <omosnacek@gmail.com>
2015-11-09 13:29:46 +0100

    Check that TZID parameter value is not a quoted string.

--- src/parameters/tzid_param.cpp
+++ src/parameters/tzid_param.cpp
@@ -12,9 +12,16 @@ Tzid_param Tzid_param::parse(const core::WithPos<core::GenericPropertyParameter>
         throw ParserException(generic.pos() , "invalid TZID parameter name");
     if(generic->getValue()->empty())
         throw ParserException(generic.pos() , "empty parameter");
-    /* tzidparam  = "TZID" "=" [tzidprefix] paramtext ??? */
+
+    auto &value = generic->getValue();
+
+    /* the spec says TZID can only contain 'paramtext' value: */
+    if (value->size() != 0 && value->front() == '"') {
+        throw ParserException(value.pos() , "The TZID parameter must not contain a quoted value!");
+    }
+
     Tzid_param x;
-    x.value = generic->getValue().value();
+    x.value = *value;
     return x;
 }
 
