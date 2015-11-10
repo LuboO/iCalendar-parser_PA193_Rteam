6895a459d56d984484bd55b65a5534c469993f95
lenka.kun <lenka.kun@gmail.com>
2015-11-10 11:32:56 +0100

    part of alarm component

--- src/components/valarm.cpp
+++ src/components/valarm.cpp
@@ -6,11 +6,48 @@ namespace components {
 const std::string VAlarm::NAME = "VALARM";
 
 void VAlarm::print(std::ostream &out) const {
-
+    out << "BEGIN:" << NAME << "\r\n";
+    for(auto &i : actionProp) i.print(out);
+    for(auto &i : triggerProp) i.print(out);
+    for(auto &i : durationProp) i.print(out);
+    for(auto &i : repeatProp) i.print(out);
+    for(auto &i : attachProp) i.print(out);
+    for(auto &i : descriptionProp) i.print(out);
+    for(auto &i : summaryProp) i.print(out);
+    for(auto &i : attendeeProp) i.print(out);
+    out << "END:" << NAME << "\r\n";
 }
 
 VAlarm VAlarm::parse(const core::WithPos<core::GenericComponent> &generic) {
-    return {};
+    if(generic->getName().value() != NAME)
+        throw ParserException(generic.pos() , "invalid name in " + NAME + " component");
+
+    VAlarm alarm;
+
+    /* Storing properties */
+    for(const core::WithPos<core::GenericProperty> &i : generic->getProperties()) {
+        if(i->getName().value() == properties::Action::NAME) {
+            alarm.actionProp.push_back(properties::Action::parse(i));
+        } else if(i->getName().value() == properties::Trigger::NAME) {
+            alarm.triggerProp.push_back(properties::Trigger::parse(i));
+        } else if(i->getName().value() == properties::DurationProp::NAME) {
+            alarm.durationProp.push_back(properties::DurationProp::parse(i));
+        } else if(i->getName().value() == properties::Repeat::NAME) {
+            alarm.repeatProp.push_back(properties::Repeat::parse(i));
+        } else if(i->getName().value() == properties::Attach::NAME) {
+            alarm.attachProp.push_back(properties::Attach::parse(i));
+        } else if(i->getName().value() == properties::Description::NAME) {
+            alarm.descriptionProp.push_back(properties::Description::parse(i));
+        } else if(i->getName().value() == properties::Summary::NAME) {
+            alarm.summaryProp.push_back(properties::Summary::parse(i));
+        } else if(i->getName().value() == properties::Attendee::NAME) {
+            alarm.attendeeProp.push_back(properties::Attendee::parse(i));
+
+        } else {
+            throw ParserException(i.pos() , "invalid property in " + NAME + " component");
+        }
+    }
+    return alarm;
 }
 
 } // namespace components
--- src/components/valarm.h
+++ src/components/valarm.h
@@ -6,11 +6,30 @@
 #include <ostream>
 #include <vector>
 
+#include "properties/action.h"
+#include "properties/trigger.h"
+#include "properties/durationprop.h"
+#include "properties/repeat.h"
+#include "properties/attach.h"
+#include "properties/description.h"
+#include "properties/summary.h"
+#include "properties/attendee.h"
+
 namespace ical {
 namespace components {
 
 class VAlarm
 {
+private:
+    /** EVENT component properties **/
+    std::vector<properties::Action> actionProp;
+    std::vector<properties::Trigger> triggerProp;
+    std::vector<properties::DurationProp> durationProp;
+    std::vector<properties::Repeat> repeatProp;
+    std::vector<properties::Attach> attachProp;
+    std::vector<properties::Description> descriptionProp;
+    std::vector<properties::Summary> summaryProp;
+    std::vector<properties::Attendee> attendeeProp;
 public:
     static const std::string NAME;
 
@@ -19,6 +38,14 @@ public:
     void print(std::ostream &out) const;
 
     static VAlarm parse(const core::WithPos<core::GenericComponent> &generic);
+    const std::vector<properties::Action> getActionProp() const noexcept{return actionProp;}
+    const std::vector<properties::Trigger> getTriggerProp() const noexcept{return triggerProp;}
+    const std::vector<properties::DurationProp> getDurationProp() const noexcept{return durationProp;}
+    const std::vector<properties::Repeat> getRepeatProp() const noexcept{return repeatProp;}
+    const std::vector<properties::Attach> getAttachProp() const noexcept{return attachProp;}
+    const std::vector<properties::Description> getDescriptionProp() const noexcept{return descriptionProp;}
+    const std::vector<properties::Summary> getSummaryProp() const noexcept{return summaryProp;}
+    const std::vector<properties::Attendee> getAttendeeProp() const noexcept{return attendeeProp;}
 };
 
 } // namespace components
