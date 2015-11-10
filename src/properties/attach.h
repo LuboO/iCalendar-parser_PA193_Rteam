0afd6efc8a72f9b38bdb2b43340d20ac9a92279c
lenka.kun <lenka.kun@gmail.com>
2015-11-10 13:43:22 +0100

    ATTACH property done

--- src/properties/attach.h
+++ src/properties/attach.h
@@ -6,12 +6,20 @@
 
 #include "core/genericproperty.h"
 #include "parserexception.h"
+#include "parameters/fmttype.h"
+#include "parameters/encoding.h"
+#include "parameters/value.h"
 
 namespace ical {
 namespace properties {
 
 class Attach
 {
+private:
+    std::string value;
+    std::vector<parameters::FmtType> fmttypeParam;
+    std::vector<parameters::Encoding> encodingParam;
+    std::vector<parameters::Value> valueParam;
 public:
     static const std::string NAME;
 
@@ -20,6 +28,11 @@ public:
     void print(std::ostream & out) const;
 
     static Attach parse(const core::WithPos<core::GenericProperty> &generic);
+
+    const std::string &getValue() const noexcept { return value; }
+    const std::vector<parameters::FmtType> &getFmtTypeParam() const noexcept { return fmttypeParam; }
+    const std::vector<parameters::Encoding> &getEncodingParam() const noexcept { return encodingParam; }
+    const std::vector<parameters::Value> &getValueParam() const noexcept { return valueParam; }
 };
 
 } // namespace properties
